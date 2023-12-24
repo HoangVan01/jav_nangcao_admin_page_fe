251d45e64407f3da1fc4dcffb1b49bc335c1020b
HoangVan01 <van956335@gmail.com>
2023-12-24 20:38:28 +0700

    U

---
+++ fusamate-fe-main/cms-fusamte/src/constants/config.js
@@ -0,0 +1,7 @@
+let BASE_URL;
+  BASE_URL = {
+    // API_V1_0: "http://localhost:8080/api",
+    API_V1_0: "http://localhost:8080",
+}
+
+export default BASE_URL;
---
+++ fusamate-fe-main/cms-fusamte/src/constants/url.js
@@ -0,0 +1,4 @@
+// export const HOST = process.env.REACT_APP_API_URL || "http://localhost:8080"
+export const HOST = process.env.REACT_APP_API_URL || "http://localhost:8080/api"
+
+export const IMAGE_HOST = HOST + 'upload/'
